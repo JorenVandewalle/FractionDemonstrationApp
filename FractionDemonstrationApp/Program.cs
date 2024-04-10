b4a94aa4ca911f83f3a84bef391b5e1e96d4f3dd
Joren Vandewalle <joren.vandewalle@gmail.com>
2024-04-10 21:51:28 +0200

    Add project files.

---
+++ FractionDemonstrationApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using FractionLibrary;
+
+namespace FractionDemonstrationApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction = new Fraction();
+            Fraction leftfraction = new Fraction(5,2);
+            Fraction rightfraction = new Fraction(7,3);
+
+            Fraction add = leftfraction.Add(rightfraction);
+            Fraction subtract = leftfraction.Subtract(rightfraction);
+            Fraction multiply = leftfraction.Multiply(rightfraction);
+            Fraction divide = leftfraction.Divide(rightfraction);
+
+            Fraction result = new Fraction();
+
+            Console.WriteLine($"Left fraction: {leftfraction.ToString()}     Right fraction: {rightfraction.ToString()}");
+
+            Console.WriteLine($"Add: {add.Simplify().ToString()}"); 
+            Console.WriteLine($"Subtract: {subtract.Simplify().ToString()}");
+            Console.WriteLine($"Multiply: {multiply.Simplify().ToString()}");
+            Console.WriteLine($"Divide: {divide.Simplify().ToString()}");
+
+            Console.WriteLine($"Reciprocal of Add: {add.Reciprocal().Simplify().ToString()}");
+            Console.WriteLine($"Invert of Add: {add.Invert().Simplify().ToString()}");
+            Console.WriteLine($"Result in double of Add: {add.Result()}");
+        }
+    }
+}
---
+++ FractionLibrary/Class1.cs
@@ -0,0 +1,119 @@
+﻿using FractionLibrary;
+
+namespace FractionLibrary
+{
+    public class Fraction
+    { 
+        private int numerator;
+        private int denominator;
+
+        public int Numerator
+        {
+            get { return numerator; }
+            set { numerator = value; }
+        }
+
+        public int Denominator
+        {
+            get { return denominator; }
+            set { if (value == 0)
+                {
+                    denominator = 1;
+                }
+                else
+                {
+                    denominator = value;
+                } ; }
+        }
+
+        public Fraction()
+        {
+            Numerator = 0;
+            Denominator = 1;
+        }
+         
+        public Fraction(int Numerator, int Denominator)
+        {
+            this.Numerator = Numerator;
+            if (Denominator != 0)
+                this.Denominator = Denominator;
+        }
+
+        public Fraction Add(Fraction right)
+        {
+            int commonDenominator = Denominator * right.Denominator;
+            int numeratorSum = (Numerator * right.Denominator) + (right.Numerator * Denominator);
+            return new Fraction(numeratorSum, commonDenominator);
+        }
+
+        public Fraction Subtract(Fraction right)
+        {
+            int commonDenominator = Denominator * right.Denominator;
+            int numeratorDifference = (Numerator * right.Denominator) - (right.Numerator * Denominator);
+            return new Fraction(numeratorDifference, commonDenominator);
+        }
+
+        public Fraction Multiply(Fraction right)
+        {
+            int multipliedNumerator = Numerator * right.Numerator;
+            int multipliedDenominator = Denominator * right.Denominator;
+            return new Fraction(multipliedNumerator, multipliedDenominator);
+        }
+
+        public Fraction Divide(Fraction right)
+        {
+            int dividedNumerator = Numerator * right.Denominator;
+            int dividedDenominator = Denominator * right.Numerator;
+            return new Fraction(dividedNumerator, dividedDenominator);
+        }
+
+        public Fraction Reciprocal()
+        {
+
+            return new Fraction(Denominator, Numerator);
+        }
+
+        public Fraction Invert()
+        {
+            return new Fraction(-Numerator, Denominator);
+        }
+
+        public Fraction Simplify()
+        {
+            int GetGreatestCommonFactor(int a, int b)
+            {
+                while (b != 0)
+                {
+                    int temp = b;
+                    b = a % b;
+                    a = temp;
+                }
+
+                return a;
+            }
+
+            int gcf = GetGreatestCommonFactor(Numerator, Denominator);
+            int simplifiedNumerator = Numerator / gcf;
+            int simplifiedDenominator = Denominator / gcf;
+
+            if (simplifiedDenominator < 0)
+            {
+                simplifiedNumerator *= -1;
+                simplifiedDenominator *= -1;
+            }
+
+            return new Fraction(simplifiedNumerator, simplifiedDenominator);
+
+        }
+
+        public double Result()
+        {
+            return (double)Numerator / Denominator;
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+    }
+}
